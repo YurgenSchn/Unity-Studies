9225234f0a4e68ed6c163bbccfc5dc697bca7451
Yurgen Schneider <yurgen.schn@gmail.com>
2023-02-01 22:30:07 -0300

    feat: XBOX Controller Input and Input Smooth Damping

--- Assets/Scripts/AreaTrigger.cs
+++ Assets/Scripts/AreaTrigger.cs
@@ -8,8 +8,17 @@ public class AreaTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         GameObject collider = other.gameObject;
-        print("aoba");
-        eventReachedGround.RaiseEvent(2f);
+
+        if (collider.tag == "Player")
+        {
+            print("[Ground Area Collider] Player Collided.");
+            print("[Ground Area Collider] Triggering the ReachedGroundEvent");
+            print("[Ground Area Collider] A random float information was sent in the event");
+            print("[Ground Area Collider] Self destroying...");
+            eventReachedGround.RaiseEvent(2f);
+            Destroy(gameObject);
+        }
+        
     }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-
+    // This script will use an event, of type Object
+    // Here, it will subscribe, and receive an Object
     public EventObject eventReachedGround;
+
+    // Values for SmoothDamp
+    private float currentLeftJoystickX;
+    private float smoothLeftJoystickX;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float joystickInputSmoothing;
+
+    [SerializeField]
+    private float playerSpeed;
+
 
     void Start()
     {
-        eventReachedGround.OnEventRaised += gritar;
+        eventReachedGround.OnEventRaised += reactToEvent;
     }
 
-    void gritar(object a)
+    // Subscriber to the ReachedGroundEvent
+    void reactToEvent(object a)
     {
-        print("Aah vou cair " + a.ToString());
+        print("[Player] This function was subscribed for the ReachedGroundEvent.");
+        print("[Player] Here's what I received: " + a.ToString());
+        print("[Player] Unsubscribing from the event.");
+        eventReachedGround.OnEventRaised -= reactToEvent; //Once triggered, unsubscribe
+
+    }
+
+    void SmoothInput()
+    {
+        float horizontal = Input.GetAxis("LeftJoystickX");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        currentLeftJoystickX = Mathf.SmoothDamp(currentLeftJoystickX, horizontal, ref smoothLeftJoystickX, joystickInputSmoothing);
+    }
+
+    void LateralMovement()
+    {
+        Vector2 currentVel = rb.velocity;
+        currentVel.x = currentLeftJoystickX * playerSpeed;
+        rb.velocity = currentVel;
+    }
+
+    void Update()
+    {
+        SmoothInput();
+        LateralMovement();
     }
 
 }
